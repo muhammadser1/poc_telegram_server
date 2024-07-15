ee5bf4b6ef804196de45a26a4314519389cb0870
muhammad_Sarahni <117571362+muhammadser1@users.noreply.github.com>
2024-07-15 23:37:57 +0300

    add error handle

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pymongo
 from fastapi import FastAPI, HTTPException, Depends
 import random
 from models import MongoDB
@@ -12,11 +13,20 @@ def test():
 
 @server.post("/random-numbers")
 def generate_and_store_random(mongodb: MongoDB = Depends(MongoDB.get_mongodb)):
-    random_number = random.randint(1, 10)
+    try:
+        random_number = random.randint(1, 100)
+        if mongodb.getNumber(random_number):
+            raise HTTPException(status_code=400, detail=f"Number {random_number} already exists in the database")
 
-    if mongodb.getNumber(random_number):
-        raise HTTPException(status_code=500, detail="Failed to store random number")
+        result = mongodb.storeNumber(random_number)
+        if result:
+            return {"message": f"Random number {random_number} stored successfully"}
 
-    result = mongodb.storeNumber(random_number)
-    if result:
-        return {"message": f"Random number {random_number} stored successfully"}
+    except pymongo.errors.OperationFailure as e:
+        raise HTTPException(status_code=500, detail=f"Failed to connect to MongoDB: {str(e)}")
+
+    except pymongo.errors.PyMongoError as e:
+        raise HTTPException(status_code=500, detail=f"PyMongo error occurred: {str(e)}")
+
+    except HTTPException as e:
+        raise e
